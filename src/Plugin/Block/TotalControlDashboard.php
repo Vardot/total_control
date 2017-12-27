53e5705b236808fa11d58fd84fdf7791a0fba25c
git <git@3097947.no-reply.drupal.org>
2017-12-27 21:32:15 +0200

    Issue by mohammad faqeh: Dashboard text should be Translatable

--- src/Plugin/Block/TotalControlDashboard.php
+++ src/Plugin/Block/TotalControlDashboard.php
@@ -31,13 +31,13 @@ class TotalControlDashboard extends BlockBase {
     if ($pm_ui_exist) {
       return array(
         '#type' => 'markup',
-        '#markup' => '<p>Welcome to your administrative dashboard.&nbsp;<a href="/admin/structure/page_manager/manage/total_control_dashboard/page_variant__total_control_dashboard-http_status_code-0__content?js=nojs">Edit this panel</a>&nbsp;to add more blocks here, or configure those provided by default.</p>'
+        '#markup' => $this->t('<p>Welcome to your administrative dashboard.&nbsp;<a href="/admin/structure/page_manager/manage/total_control_dashboard/page_variant__total_control_dashboard-http_status_code-0__content?js=nojs">Edit this panel</a>&nbsp;to add more blocks here, or configure those provided by default.</p>')
       );
     }
     else {
       return array(
         '#type' => 'markup',
-        '#markup' => '<p>Welcome to your administrative dashboard.&nbsp;You have to enable <strong>page manager ui</strong> module to edit this panel.</p>'
+        '#markup' => $this->('<p>Welcome to your administrative dashboard.&nbsp;You have to enable <strong>page manager ui</strong> module to edit this panel.</p>')
       );
     }
   }
