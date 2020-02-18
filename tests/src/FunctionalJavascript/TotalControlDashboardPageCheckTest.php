f06c82001fd07a8f692b57960a07096167cd4a25
Rajab Natshah <rajabn@gmail.com>
2020-02-18 16:08:19 +0200

    Issue #3069156: Add Automated Testing for the [Total Control Admin Dashboard] module

--- tests/src/FunctionalJavascript/TotalControlDashboardPageCheckTest.php
+++ tests/src/FunctionalJavascript/TotalControlDashboardPageCheckTest.php
@@ -20,45 +20,28 @@ class TotalControlDashboardPageCheckTest extends WebDriverTestBase {
    * @var array
    */
   public static $modules = [
+    'system',
+    'node',
     'user',
+    'comment',
+    'taxonomy',
     'filter',
     'text',
+    'link',
     'toolbar',
+    'block',
+    'block_content',
+    'views',
+    'views_ui',
     'contextual',
-    'menu_link_content',
-    'field',
-    'field_ui',
-    'filter',
-    'history',
-    'dynamic_page_cache',
-    'page_cache',
-    'menu_ui',
-    'node',
     'ctools',
     'ctools_block',
     'ctools_views',
     'layout_discovery',
-    'block',
-    'block_content',
-    'contextual',
-    'views_ui',
-    'views',
-    'page_manager',
-    'page_manager_ui',
     'panels',
-    'taxonomy',
-    'comment',
+    'page_manager',
     'total_control',
   ];
-
-  /**
-   * The profile used during tests.
-   *
-   * This purposefully uses the standard profile.
-   *
-   * @var string
-   */
-  public $profile = 'standard';
 
   /**
    * A user with the 'have total control' permission.
@@ -68,34 +51,41 @@ class TotalControlDashboardPageCheckTest extends WebDriverTestBase {
   protected $webUser;
 
   /**
-   * {@inheritdoc}
+   * Tests Total Control Dashboard Page Check.
    */
-  protected function setUp() {
-    parent::setUp();
+  public function testTotalControlDashboardPageCheck() {
 
     $permissions = [
-      'administer actions',
       'administer content types',
-      'administer content',
+      'administer menu',
       'administer comments',
-      'administer vocabularies and terms',
+      'administer taxonomy',
       'administer users',
-      'access comments',
-      'access content',
-      'access toolbar',
-      'view the administration theme',
-      'have total control',
       'administer pages',
+      'administer blocks',
+      'administer views',
+      'administer panels layouts',
+      'administer panels styles',
+      'administer pane access',
+      'administer advanced pane settings',
+      'access administration pages',
+      'access user profiles',
+      'access content',
+      'access comments',
+      'access toolbar',
+      'access contextual links',
+      'view the administration theme',
+      'view pane admin links',
+      'use panels caching features',
+      'use panels dashboard',
+      'use panels locks',
+      'use ipe with page manager',
+      'have total control',
     ];
 
     $this->webUser = $this->drupalCreateUser($permissions);
     $this->drupalLogin($this->webUser);
-  }
 
-  /**
-   * Tests Total Control Dashboard Page Check.
-   */
-  public function testTotalControlDashboardPageCheck() {
     $this->drupalGet('admin');
     $this->assertSession()->waitForElementVisible('css', '#toolbar-link-page_manager-page_view_total_control_dashboard_total_control_dashboard-http_status_code-0');
     $dashboard_toolbar_text = $this->t('Dashboard');
